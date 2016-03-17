89611843d2df34449da562c46a13f84ccda241d0
Marcus Haagh <haagh9@hotmail.com>
2016-03-17 21:18:08 +0100

    started working on example

---
+++ FSharp.MachineLearning/MachineLearningTest/DatingSite.fs
@@ -0,0 +1,18 @@
+﻿namespace MachineLearningTestCases
+
+module DatingSite =
+    open System.IO
+
+    let readLines (filepath : string) = File.ReadAllLines(filepath)
+
+    let dataPath = "C:\Users\Marcus\Documents\GitHub\machinelearninginaction\Ch02\datingTestSet.txt";
+    let testPath = "C:\Users\Marcus\Documents\GitHub\machinelearninginaction\Ch02\datingTestSet2.txt"
+
+    let run () =
+        let data =
+            readLines dataPath
+            |> Seq.toList
+            |> List.map (fun x -> x.Split [|'t'|])
+            |> List.map List.ofArray
+            |> List.map (fun x -> List.map float x)
+        data
